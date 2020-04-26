5da0e02a00284e1b47e1e4976a1e311d34dd9285
孔鹏飞 <pengfei.kong@unspay.com>
2020-04-26 17:06:18 +0800

    update

--- app/src/main/java/com/github/kongpf8848/animation/fragment/TitleFragment.java
+++ app/src/main/java/com/github/kongpf8848/animation/fragment/TitleFragment.java
@@ -6,6 +6,12 @@ import com.github.kongpf8848.animation.R;
 import com.github.kongpf8848.xsdk.ui.fragment.BaseFragment;
 import com.github.kongpf8848.xsdk.ui.fragment.BaseLazyFragment;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
 import butterknife.BindView;
 
 public class TitleFragment extends BaseLazyFragment {
@@ -23,5 +29,8 @@ public class TitleFragment extends BaseLazyFragment {
     protected void onFragmentFirstVisible() {
         super.onFragmentFirstVisible();
         tv_title.setText(getArguments().getString("title",""));
+
     }
+
+
 }
