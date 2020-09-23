cecd650dd5f4ec9718faf6145a0bc00b4ea2ae0d
kongpengfei <kongpf8848@gmail.com>
2020-09-23 10:28:20 +0800

    添加lottie动画

--- app/src/main/java/com/github/kongpf8848/animation/activity/LottieAnimActivity.java
+++ app/src/main/java/com/github/kongpf8848/animation/activity/LottieAnimActivity.java
@@ -5,6 +5,7 @@ import android.view.View;
 import com.github.kongpf8848.animation.R;
 import com.github.kongpf8848.animation.activity.lottie.AutoHomeActivity;
 import com.github.kongpf8848.animation.activity.lottie.BossActivity;
+import com.github.kongpf8848.animation.activity.lottie.CardActivity;
 
 import butterknife.OnClick;
 
@@ -23,5 +24,10 @@ public class LottieAnimActivity extends BaseActivity {
     @OnClick(R.id.button2)
     public void onButton2(View view) {
         startActivity(BossActivity.class);
+    }
+
+    @OnClick(R.id.button3)
+    public void onButton3(View view) {
+        startActivity(CardActivity.class);
     }
 }
