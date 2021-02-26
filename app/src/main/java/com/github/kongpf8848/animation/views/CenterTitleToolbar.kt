35fc477638b936cae13f321a9bf8d1f00423712f
kongpengfei <kongpf8848@gmail.com>
2021-02-26 10:46:30 +0800

    优化CenterTitleToolbar

--- app/src/main/java/com/github/kongpf8848/animation/views/CenterTitleToolbar.kt
+++ app/src/main/java/com/github/kongpf8848/animation/views/CenterTitleToolbar.kt
@@ -5,23 +5,16 @@ import android.content.res.ColorStateList
 import android.text.TextUtils
 import android.util.AttributeSet
 import android.view.Gravity
-import android.view.MotionEvent
 import android.view.ViewGroup
-import android.widget.ImageButton
 import android.widget.TextView
 import androidx.annotation.NonNull
+import androidx.annotation.StyleRes
 import androidx.appcompat.R
 import androidx.appcompat.widget.AppCompatTextView
 import androidx.appcompat.widget.Toolbar
+import androidx.core.widget.TextViewCompat
 
-/**
- * Title居中的Toolbar
- */
-class CenterTitleToolbar @JvmOverloads constructor(
-        context: Context,
-        attrs: AttributeSet? = null,
-        defStyleAttr: Int = 0
-) : Toolbar(context, attrs, defStyleAttr) {
+class CenterTitleToolbar @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0) : Toolbar(context, attrs, defStyleAttr) {
 
     private var mTitleTextAppearance = 0
     private var mTitleTextColor: ColorStateList? = null
@@ -29,64 +22,42 @@ class CenterTitleToolbar @JvmOverloads constructor(
     private var mTitleText: CharSequence? = null
 
     init {
+
         val ta = getContext().obtainStyledAttributes(attrs, R.styleable.Toolbar, defStyleAttr, 0)
         mTitleTextAppearance = ta.getResourceId(R.styleable.Toolbar_titleTextAppearance, 0)
         mTitleText = ta.getText(R.styleable.Toolbar_title)
         if (!TextUtils.isEmpty(mTitleText)) {
-            setTitle(mTitleText)
+            title = mTitleText
         }
         if (ta.hasValue(R.styleable.Toolbar_titleTextColor)) {
             setTitleTextColor(ta.getColorStateList(R.styleable.Toolbar_titleTextColor)!!)
         }
         ta.recycle()
-
-        val fieldNav = Toolbar::class.java.getDeclaredField("mNavButtonView")
-        fieldNav.isAccessible = true
-        val navButton=fieldNav.get(this) as ImageButton?
-        fieldNav.isAccessible=false
-        navButton?.apply {
-            setOnTouchListener { _, event ->
-                when (event.action) {
-                    MotionEvent.ACTION_DOWN -> {
-                        animate().scaleX(1.3f).scaleY(1.3f).setDuration(400).start()
-                    }
-                    MotionEvent.ACTION_UP, MotionEvent.ACTION_CANCEL -> {
-                        animate().scaleX(1.0f).scaleY(1.0f).setDuration(400).start()
-                    }
-                }
-                false
-            }
-        }
-
     }
 
     override fun setTitle(title: CharSequence?) {
-
-        if (mTitleTextView == null) {
-            mTitleTextView = AppCompatTextView(context)
-            mTitleTextView?.apply {
-                isSingleLine = true
-                ellipsize = TextUtils.TruncateAt.END
-                addView(
-                        this,
-                        LayoutParams(
-                                ViewGroup.LayoutParams.WRAP_CONTENT,
-                                ViewGroup.LayoutParams.WRAP_CONTENT,
-                                Gravity.CENTER
-                        )
-                )
+        if (!TextUtils.isEmpty(title)) {
+            if (mTitleTextView == null) {
+                mTitleTextView = AppCompatTextView(context).apply {
+                    isSingleLine = true
+                    ellipsize = TextUtils.TruncateAt.END
+                    addView(this, LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT, Gravity.CENTER))
+                }
+            }
+            if (mTitleTextAppearance != 0) {
+                mTitleTextView?.apply {
+                    TextViewCompat.setTextAppearance(this,mTitleTextAppearance)
+                }
+            }
+            if (mTitleTextColor != null) {
+                mTitleTextView?.setTextColor(mTitleTextColor)
             }
         }
-        if (mTitleTextAppearance != 0) {
-            mTitleTextView?.setTextAppearance(context, mTitleTextAppearance)
+        else if(mTitleTextView!=null){
+            removeView(mTitleTextView)
         }
         mTitleTextView?.text = title
         mTitleText = title
-
-    }
-
-    override fun setTitle(resId: Int) {
-        setTitle(getContext().getText(resId))
     }
 
     override fun getTitle(): CharSequence? {
@@ -98,4 +69,10 @@ class CenterTitleToolbar @JvmOverloads constructor(
         mTitleTextView?.setTextColor(color)
     }
 
+    override fun setTitleTextAppearance(context: Context, @StyleRes resId: Int) {
+        mTitleTextAppearance = resId
+        mTitleTextView?.apply {
+            TextViewCompat.setTextAppearance(this,resId)
+        }
+    }
 }
